efbf89c30496fde8a3d65961487e49eccdee3d7a
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-03-04 12:54:46 -0500

    update

--- rust_in_action.mm
+++ rust_in_action.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Rust In Action&#xa;Rust实战" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1709572389770">
+<node TEXT="Rust In Action&#xa;Rust实战" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1709573062628" LINK="https://www.rust-lang.org/">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
 <hook NAME="MapStyle" background="#fdf6e3" zoom="0.826">
@@ -82,5 +82,200 @@
 </stylenode>
 </map_styles>
 </hook>
+<node TEXT="第一部分 Rust语言的特色" POSITION="bottom_or_right" ID="ID_983572525" CREATED="1709572593271" MODIFIED="1709572856474"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第一部分介绍Rust的语法和一些独特的特点
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="第1章 Rust语言介绍" ID="ID_1734025972" CREATED="1709572607427" MODIFIED="1709572812704"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第1章解释Rust存在的原因，以及如何开始用它来编程。
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="1.1 哪些地方使用了Rust？" ID="ID_909200895" CREATED="1709572992155" MODIFIED="1709573003037"/>
+<node TEXT="1.2 在工作中提倡使用Rust" ID="ID_224371999" CREATED="1709573024885" MODIFIED="1709573036303"/>
+<node TEXT="1.3 Rust初体验" ID="ID_683890878" CREATED="1709573104234" MODIFIED="1709573108989"/>
+<node TEXT="1.4 下载本书源代码" ID="ID_1149763821" CREATED="1709573530288" MODIFIED="1709573539003"/>
+<node TEXT="1.5 使用Rust语言的感受" ID="ID_969528233" CREATED="1709573539313" MODIFIED="1709573547159"/>
+<node TEXT="1.6 Rust语言是什么?" ID="ID_286760021" CREATED="1709573547674" MODIFIED="1709573554343"/>
+<node TEXT="1.7 Rust的主要特点" ID="ID_1105960409" CREATED="1709573554545" MODIFIED="1709573566078"/>
+<node TEXT="1.8 Rust的缺点" ID="ID_716633968" CREATED="1709573566263" MODIFIED="1709573572976"/>
+<node TEXT="1.9 TLS安全性问题的研究" ID="ID_1036955747" CREATED="1709573573608" MODIFIED="1709573583331"/>
+<node TEXT="1.10 Rust最适合哪些邻域" ID="ID_1750594759" CREATED="1709573583494" MODIFIED="1709573595387"/>
+<node TEXT="1.11 Rust的隐式特性" ID="ID_533030976" CREATED="1709573595557" MODIFIED="1709573607579"/>
+<node TEXT="1.12 Rust术语表" ID="ID_1816082481" CREATED="1709573607740" MODIFIED="1709573618019"/>
+</node>
+<node TEXT="第2章 Rust语言基础" ID="ID_333882076" CREATED="1709572626874" MODIFIED="1709572828851"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第2章提供翔实的Rust语法基础知识。本章示例包括芒德布罗集渲染器和一个grep的克隆。
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="2.1 创建一个可运行的程序" ID="ID_1179913266" CREATED="1709573621469" MODIFIED="1709573632138"/>
+</node>
+<node TEXT="第3章 复合数据类型" ID="ID_1196240036" CREATED="1709572639675" MODIFIED="1709572849380"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第3章讲解如何组合Rust的数据类型以及如何使用一些错误处理的工具。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第4章 生命周期、所有权和借用" ID="ID_1917718990" CREATED="1709572650641" MODIFIED="1709572868092"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第4章讨论Rust中确保要访问的数据始终有效的机制。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="第二部分 揭开系统编程的神秘面纱" POSITION="bottom_or_right" ID="ID_992239694" CREATED="1709572667318" MODIFIED="1709572875882"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第二部分是将Rust应用于系统编程领域的一些基础性介绍。
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="第5章 深入理解数据" ID="ID_302762253" CREATED="1709572681066" MODIFIED="1709572881826"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第5章介绍在数字计算机中信息是如何表示的，重点介绍数字是如何被近似表示的。本章示例包括实现定点数格式和一个CPU模拟器。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第6章 内存" ID="ID_243345895" CREATED="1709572696168" MODIFIED="1709572887964"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第6章阐释引用、指针、虚拟内存、栈和堆等术语。本章示例包括一个内存扫描器和自动生成艺术项目。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第7章 文件与存储" ID="ID_1824840070" CREATED="1709572702375" MODIFIED="1709572894108"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第7章阐释如何将数据结构存储到存储设备中。本章示例包括一个hexdump的克隆和一个可运行的数据库。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第8章 网络" ID="ID_991294946" CREATED="1709572710990" MODIFIED="1709572898225"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第8章通过多次重新实现HTTP讲解计算机是如何进行通信的，每一次实现都会剥离掉一个抽象层。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第9章 时间与时间保持" ID="ID_280835805" CREATED="1709572721512" MODIFIED="1709572904360"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第9章探索在数字计算机中跟踪时间的过程。本章示例包括一个可运行的NTP客户端。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第10章 进程、线程和容器" ID="ID_1826236304" CREATED="1709572733794" MODIFIED="1709572915206"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第10章介绍进程、线程和一些相关的抽象概念。本章示例包括一个海龟绘图应用程序和一个并行解析器。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第11章 内核" ID="ID_120168388" CREATED="1709572746145" MODIFIED="1709572920882"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第11章介绍操作系统的作用和计算机是如何启动的。本章示例包括编译自己的引导加载程序和操作系统内核。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="第12章 信号、中断和异常" ID="ID_163153660" CREATED="1709572752613" MODIFIED="1709572922736"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      第12章阐释外部世界是如何与CPU和操作系统进行通信的。
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </map>
