f503788ffadc21a76d5e93ae4a50a5d1fdefbdc4
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-03-05 13:35:52 -0500

    add 2.2

---
+++ part1/ch02/2.2/numeric/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let a = 10;
+    let b: i32 = 20;
+    let c = 30i32;
+    let d = 30_i32;
+
+    let e = add(add(a,b), add(c,d));
+
+    println!("({0}+{1})+({2}+{3}) = {4}", a, b, c, d, e)
+}
+
+fn add(i: i32, j: i32) -> i32 {
+    i + j
+}
